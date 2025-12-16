21dd9c3373c18d6693cfd84cecc7b38c1a8a1f7c
ASTARZ300 <astarz300@caledonian.ac.uk>
2025-12-16 11:12:58 +0000

    update projectRoutes.js

--- routes/projectRoutes.js
+++ routes/projectRoutes.js
@@ -3,20 +3,20 @@ const router = express.Router();
 
 // HOME
 router.get('/', (req, res) => {
-    res.render('index');   // ✅ masz index.mustache
+    res.render('index');   
 });
 
 // USER
 router.get('/login', (req, res) => {
-    res.render('user/login');   // ✅ PODKATALOG
+    res.render('user/login');   
 });
 
 router.get('/register', (req, res) => {
-    res.render('user/register'); // ✅ PODKATALOG
+    res.render('user/register'); 
 });
 
 router.get('/about', (req, res) => {
-    res.render('aboutUs'); // ✅ masz aboutUs.mustache
+    res.render('aboutUs'); 
 });
 
 module.exports = router;
